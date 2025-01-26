a399de97944b038c60add2421f2b34a1cf4b4a06
Maksim Kozlov <maalkozlov@edu.hse.ru>
2025-01-26 17:23:28 +0300

    Probably hooking libs correctly now

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
-#include <QApplication>
-#include <QLabel>
+#include <Eigen/Dense>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+int main() {
+    Eigen::Matrix2d mat_a;
+    mat_a << 1, 2,
+            3, 4;
 
-    QLabel label("Hello, Qt!");
-    label.resize(400, 300);
-    label.show();
+    Eigen::Matrix2d mat_b;
+    mat_b << 5, 6,
+            7, 8;
 
-    return app.exec();
+    Eigen::Matrix2d mat_sum = mat_a + mat_b;
+    Eigen::Matrix2d mat_product = mat_a * mat_b;
+
+    std::cout << "Matrix A:\n" << mat_a << "\n\n";
+    std::cout << "Matrix B:\n" << mat_b << "\n\n";
+    std::cout << "Sum (A + B):\n" << mat_sum << "\n\n";
+    std::cout << "Product (A * B):\n" << mat_product << "\n";
+    return 0;
 }
