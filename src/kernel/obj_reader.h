d0bdc5aac1baaa5f48922883771813457b4cce75
Maksim Kozlov <maalkozlov@edu.hse.ru>
2026-02-02 01:50:57 +0300

    Using Assimp lib to parse .obj

--- src/kernel/obj_reader.h
+++ src/kernel/obj_reader.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <filesystem>
-#include <string>
 #include <vector>
 
 #include "triangle.h"
@@ -10,14 +9,8 @@ namespace renderer::kernel {
 
 class ObjReader {
  public:
-  static std::vector<Triangle> ReadFromFile(const std::filesystem::path& filepath);
-
- private:
-  static Point3 ParseVertex(const std::string& line);
-  static Vector3 ParseNormal(const std::string& line);
-  static std::vector<Triangle> ParseFace(const std::string& line,
-                                         const std::vector<Point3>& vertices,
-                                         const std::vector<Vector3>& normals);
+  static std::vector<Triangle> ReadFromFile(
+      const std::filesystem::path& filepath);
 };
 
-}  // namespace renderer
+}  // namespace renderer::kernel
