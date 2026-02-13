85ef5f7206f09cca56925d7923fe918e67bb05df
Maksim Kozlov <maalkozlov@edu.hse.ru>
2026-02-13 15:19:23 +0300

    Added templated constructor to alias

--- src/util/alias.h
+++ src/util/alias.h
@@ -8,9 +8,13 @@ template <typename T, class Tag>
   requires std::is_arithmetic_v<T>
 class Alias {
  public:
-  explicit constexpr Alias(T value) : value_(value) {}
+  explicit constexpr Alias(T value) noexcept : value_(value) {}
 
-  constexpr T operator()() const { return value_; }
+  template <typename U>
+    requires std::is_arithmetic_v<U> && std::is_convertible_v<U, T>
+  explicit constexpr Alias(U value) noexcept : value_(static_cast<T>(value)) {}
+
+  constexpr T operator()() const noexcept { return value_; }
 
  private:
   T value_;
